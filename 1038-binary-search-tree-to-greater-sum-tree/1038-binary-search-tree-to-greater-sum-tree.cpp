4259e4d5e249a0e5ea791a38ed0265d3694f364e
Oarisa Rebayet <151024492+OarisaR@users.noreply.github.com>
2026-04-25 00:05:40 +0600

    Time: 0 ms (100%), Space: 11.4 MB (47.8%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void rev_inorder(TreeNode* root, int& sum) {
+        if (root == NULL)
+            return;
+        rev_inorder(root->right, sum);
+        sum = sum + root->val;
+        root->val = sum;
+        rev_inorder(root->left, sum);
+    }
+    TreeNode* bstToGst(TreeNode* root) {
+        int sum = 0;
+        rev_inorder(root, sum);
+        return root;
+    }
+};
