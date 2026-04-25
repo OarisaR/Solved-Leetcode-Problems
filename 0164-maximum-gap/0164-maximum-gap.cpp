7690ebc86cd0b69d719d82efa84276db853538db
Oarisa Rebayet <151024492+OarisaR@users.noreply.github.com>
2026-04-25 10:27:55 +0600

    Time: 39 ms (63.59%), Space: 85.8 MB (75.01%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.cpp
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -6,7 +6,7 @@ public:
         int mx = 0;
         int sum = 0;
         for (int i = 0; i < n - 1; i++) {
-            sum = nums[i + 1 - nums[i]
+            sum = nums[i + 1] - nums[i];
             mx = max(mx, sum);
         }
         return mx;
