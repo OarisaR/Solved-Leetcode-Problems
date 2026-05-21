9360033fb5d0771d9a17aaf7d6c30dfc698c1d1d
Oarisa Rebayet <151024492+OarisaR@users.noreply.github.com>
2026-05-21 20:29:49 +0600

    Time: 0 ms (100%), Space: 80.5 MB (72.66%) - LeetHub

--- 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     vector<int> findArray(vector<int>& pref) {
+        //[5,2,0,3,1]
+        //Output: [5,7,2,3,2]
+
+        // 5 = a0
+        // 2 = a0 ^ a1
+        // 0 = a0 ^ a1 ^ a3  = 2 ^ a3. how to find a3 then?
+        // 2 ^ 0 = a3; [similar ones get cancelled out] in this way we can find a1,a2,a3...
         vector<int> ans(pref.size());
         ans[0] = pref[0];
         for(int i = 1; i < pref.size() ; i++){
