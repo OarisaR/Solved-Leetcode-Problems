4a56bbfbbc072c932e501e1f80440cf25e1038f2
Oarisa Rebayet <151024492+OarisaR@users.noreply.github.com>
2026-05-27 14:10:26 +0600

    Time: 71 ms (56.46%), Space: 24.5 MB (39.89%) - LeetHub

---
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
@@ -0,0 +1,32 @@
+/*
+- special character means which has both lowercase and uppercase
+- and lowercase appears first!
+- so all lowercase must appear to the left of first uppercase
+- so find first occurence of uppercase letter
+ - find last occurence of lowercase
+ - if upper one > lower  = then cnt++;
+*/
+
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        int cnt = 0;
+        vector<int> firstupp(26, -1);
+        vector<int> lastlow(26, -1);
+        for (int i = 0; i < word.size(); i++) {
+            if (islower(word[i])) {
+                lastlow[word[i] - 'a'] = i;
+            }
+        }
+        for (int i = 0; i < word.size(); i++) {
+            if (isupper(word[i]) && firstupp[word[i] - 'A'] == -1) {
+                firstupp[word[i] - 'A'] = i;
+            }
+        }
+        for (int i = 0; i < 26; i++) {
+            if (firstupp[i]!=-1 && lastlow[i]!=-1 && firstupp[i] > lastlow[i])
+               { cnt++;}
+        }
+        return cnt;
+    }
+};
