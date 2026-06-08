d7c77f6af9c2eb9f6ead233316995db830994683
Oarisa Rebayet <151024492+OarisaR@users.noreply.github.com>
2026-06-08 16:40:08 +0600

    Time: 11 ms (30.45%), Space: 133.3 MB (62.93%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -1,29 +1,28 @@
 /*
 - First thought is quicksort, which arranges elements based on a pivot
+- (< pivot)   (== pivot)   (> pivot)
 */
 
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int> left;
-        vector<int> right;
+        vector<int> ans;
         int n = nums.size();
-        int cnt = 0;
+
         for (int i = 0; i < n; i++) {
             if (nums[i] < pivot)
-                left.push_back(nums[i]);
-
-            else if (nums[i] > pivot)
-                right.push_back(nums[i]);
-            else
-                cnt++; // counts number of pivot
+                ans.push_back(nums[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == pivot)
+                ans.push_back(pivot);
         }
 
-        for (int i = 0; i < cnt; i++) {
-            left.push_back(pivot);
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > pivot)
+                ans.push_back(nums[i]);
         }
 
-        left.insert(left.end(), right.begin(), right.end());
-        return left;
+       return ans;
     }
 };
