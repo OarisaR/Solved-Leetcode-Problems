1be521b692a5b46f3361aa1f33ccda01e911dbe4
Oarisa Rebayet <151024492+OarisaR@users.noreply.github.com>
2026-07-07 10:14:18 +0600

    Time: 67 ms (8.57%), Space: 33.6 MB (5.19%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        vector<int> res;
+        int n = intervals.size();
+        auto lambda = [](vector<int> vec1, vector<int> vec2) {
+            if (vec1[0] == vec2[0]) {
+                return vec1[1] > vec2[1];
+                // if start is same, we choose the one with farthest endpoint
+                // for a longer interval
+            }
+            return vec1[0] < vec2[0];
+        } ;sort(intervals.begin(), intervals.end(), lambda);
+        int cnt = 1; // one is always taken.
+        int lastEnd = intervals[0][1];
+        for (int i = 0; i < n; i++) {
+            if(intervals[i][1] > lastEnd){
+                lastEnd = intervals[i][1]; // new end updated
+                cnt++;
+            }
+            else continue;
+        }
+        return cnt;
+    }
+};
