35b681d59e50706fa0e5e8c793254bef61df4109
Oarisa Rebayet <151024492+OarisaR@users.noreply.github.com>
2026-09-08 12:01:32 +0600

    Time: 0 ms (100%), Space: 8.5 MB (84.49%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,21 @@
+/*
+- FOR 4 DIGIT --> 1 COMMA , EG. 1,000
+- FOR 5 DIGIT --> 1 COMMA E.G 24,000
+- FOR 6 DIGIT --> 1 COMMA E.G. 324,000 ---> 1,000 - 324,000 ; 325000 zeroes!
+*/
+
+
+class Solution {
+public:
+    int countCommas(int n) {
+        // count any remainder
+        if(n < 1000) return 0;
+
+
+        int rem = n % 1000; // 19
+        n-=rem; // 2000
+
+        // answer is remainder + n + 1
+        return n == 1000 ? rem + 1 : (n - 1000) + 1 + rem;
+    }
+};
